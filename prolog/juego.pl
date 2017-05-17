a2727718f1c11ec3a4d710adc1cb1767d6035b01
Juan García Garland <jpgarcia@fing.edu.uy>
2017-05-17 11:30:31 -0300

    decidido modelado de tablero, implementa parcialmente estado_inicial/1

--- prolog/juego.pl
+++ prolog/juego.pl
@@ -9,7 +9,7 @@
 :- use_module(lab1).
 % probablemente tengamos que usar los predicados de matrices del lab1,
 % por ahora lo importé y estoy usando el mio en mi repo (sin pushearlo,
-% despues vemos cual usamos y eso -el que anda más rápido-)
+% despues vemos cual usamos y eso -el que anda más rápido?-)
 
 
 %% cantidad_casilleros(?Ancho,?Alto)
@@ -29,15 +29,56 @@ cantidad_casilleros(8,10).
 % La pelota comienza en la posición p(0,0), que es la posición central
 % del tablero.
 % El turno inicial es del jugador 1, que es el que patea hacia arriba.
+%
+% La pelota se representa (aca) con coordenadas del tablero,
+% despues hacia afuera las mandamos con el sistema que la interfaz requiere
+% TODO: la posicion inicial (5,5) esta hardcodeada, hay que calcular el centro
+% en función de las dimensiones del tablero
+%% representamos a los vértices con un booleano que indica si fue visitado
+% y una lista de las direcciones a las que se puede ir desde el mismo
 
-estado_inicial(e). % TODO
+estado_inicial(estado(Tablero,pelota(5,5),turno(jugador1))) :-
+    cantidad_casilleros(K,L),
+    M is L+3,  % modifico las dimensiones porque cuento vertices, y porque
+    N is K+1,  % hay una fila mas atrasde cada arco
+    inicial_medio(Medio),
+    matriz_f(M,N,Medio,Tablero),
+    inicializar_bandas(Tablero). %corte elimina choicepoints
+
+
+%% inicializar_bandas(+Tablero)
+% -predicado con efecto, iteración loopfail-
+% Setea el estado de las bandas del tablero, oeste y este
+% TODO : parametrizar segun el tamaño del tablero
+inicializar_bandas(Tablero) :-
+    inicial_oeste(Oeste),
+    inicial_este(Este),
+    between(3,11,F),
+    nuevo_valor_celda_f(F,1,Tablero,Oeste),
+    nuevo_valor_celda_f(F,9,Tablero,Este),
+    fail.
+
+inicializar_bandas(_).
+
+
+%% inicial_medio(?Vertice)
+% predicado que define las casillas iniciales del medio del tablero
+inicial_medio(vertice(false,[n,ne,e,se,s,sw,w,nw])).
+
+%% inicial_{oeste|este}(?Vertice)
+% predicado que define las casillas iniciales del {oeste|este} del tablero
+inicial_oeste(vertice(true,[n,ne,e,se,s])).
+inicial_este(vertice(true,[n,s,sw,w,nw])).
 
 
 %% posicion_pelota(+E,?P)
 %
 % P es la posición de la pelota para el estado E.
 
-posicion_pelota(_,p(0,0)). % TODO
+posicion_pelota(estado(_,pelota(M,N),_),p(X,Y)) :-
+    X is M-5,
+    Y is N-5.
+    % TODO : esto no es parametrico con la dimension del tablero
 
 
 %% mover(+E,?LP,?E2)
@@ -74,3 +115,34 @@ turno(_,1). % TODO
 % (Se usa para validar las jugadas de un jugador humano)
 
 prefijo_movimiento(_,_). % TODO
+
+
+
+
+
+
+%% para testear
+print_Estado(estado(Board,_,_)) :-
+    between(1,13,F),
+    nl,
+    between(1,9,C),
+    valor_celda_f(F,C,Board,V),
+    print_cell(V),
+    fail.
+print_Estado(_).
+
+print_cell(vertice(false,[])) :- write('_').
+print_cell(vertice(false,[n,ne,e,se,s,sw,w,nw])) :-
+    write('┼'). %unicode 253C
+print_cell(vertice(false,[n,ne,e,se,s])) :-
+    write('├').
+
+
+%% unit test suite
+:- begin_tests(juego).
+
+test(posicion_pelota_inicial) :-
+    estado_inicial(E),
+    posicion_pelota(E,p(0,0)).
+
+:- end_tests(juego).

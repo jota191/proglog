07eac0f6a6d6a2c0aacde9eb455f19884fa225c2
Juan García Garland <jpgarcia@fing.edu.uy>
2017-06-07 11:22:10 -0300

    documenta minimax

--- prolog/inteligencia.pl
+++ prolog/inteligencia.pl
@@ -35,19 +35,45 @@ hacer_jugada(E,LP,E2):- niveles_minimax(N),
 
 
 
+%% max(+Listamvs, -Mov, -V)
+%
+% Listamovs es una lista de functores mv(Mov,V) en donde Mov es un movimiento
+% (movimiento = lista de posiciones)
+% y V el valor del movimiento (listamovs viene a representar la sintetizacion
+% de todos los hijos de un nodo de minimax).
+% de la entrada mv(Mov,V) con Mayor V, se retornan Mov y V
+
 max([mv(Mov,V)],Mov,V).
 max([mv(Mov,V)|T],BM,Max) :-
     max(T,Bm2,Max2),
     (Max2>V -> (BM = Bm2,Max = Max2);(BM=Mov,Max = V)).
 
+
+% analogo a Max, retorna el menor V de la lista con su correspondiente Mov
 min([mv(Mov,V)],Mov,V).
 min([mv(Mov,V)|T],BM,Min) :-
     min(T,Bm2,Min2),
     (Min2<V -> (BM = Bm2,Min = Min2);(BM=Mov,Min = V)).
 
 
-minimax(E,Prof,Is_Maximizing,BestMov,Value) :-
-    Prof>0,
+
+
+%% minimax (+E     : Estado,
+%           +Prof  : Int,
+%           +Is_Maximizing : Bool,
+%           BestMov : movimiento (lista de posiciones)),
+%           Value   : Int
+%
+% BestMov es el movimiento optimo segun el algoritmo de minimax,
+% y tiene un valor V según la función de evaluación.
+% Is_Maximizing indica si el nodo es min o max,
+% E es el estado en el nodo donde se llama a la función
+% Prof es la profundidad
+% Jugador indica hacia donde "patea" la IA
+% (todavia no se usa porque esta hardcodeado hacia abajo)
+
+minimax(E,Prof,Is_Maximizing,BestMov,Value,Jugador) :-
+    Prof > 0,
     NewProf is Prof-1,
     findall(mv(Mov,0),mover(E,Mov,_),MovsPosibles),
     length(MovsPosibles,Len),print(Len),
@@ -57,6 +83,8 @@ minimax(E,Prof,Is_Maximizing,BestMov,Value) :-
     (Is_Maximizing = true -> max(Movs,BestMov,Value)
      ;min(Movs,BestMov,Value)),!.
 
+% Si la profundidad es 0 (llegue al ultimo nivel),
+% (se evalua la funcion)
 minimax(E,0,_Is_Maximizing,_,Value) :-
     posicion_pelota(E,p(_X,Y)),
     juego:cantidad_casilleros(H,_V),
